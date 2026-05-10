7c1cbcccb418048583d8cd02ac9b567e0b9b9e84
Harold Esteban Pedraza Murcia <145359297+2harold2@users.noreply.github.com>
2026-05-10 11:10:00 -0500

    migracion cambio

---
+++ Infrastructure/Migrations/20260510160739_UniqueNameCustomer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueNameCustomer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Customer_Name",
+                table: "Customer",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Customer_Name",
+                table: "Customer");
+        }
+    }
+}
